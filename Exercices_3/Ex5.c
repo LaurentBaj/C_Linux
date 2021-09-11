6666e9c040036ed5e96685aae8c46ab76f46d04e
LaurentBaj <laurent_baj@hotmail.com>
2021-09-11 06:59:38 -0700

    Kinda finished Ex5

--- Exercices_3/Ex5.c
+++ Exercices_3/Ex5.c
@@ -3,27 +3,38 @@
 #include <time.h>
 
 int main () {
-	/*	Create random number 
+	/*	
+	Create random number 
 	srand(time(NULL));
 	int a = rand() % 100;
 	printf("%d", a)*/;
 	
+	
+   srand(time(NULL)); 
+   int a = rand()%20;
+   if(a == 1){
+      printf("%d is 1\n", a);}
+   else if(a%2 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%3 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%5 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%7 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%11 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%13 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%17 == 0){
+      printf("%d is not a prime\n", a);}
+   else if(a%19 == 0){
+      printf("%d is not a prime\n", a);}
+   else{
+      printf("%d is a prime\n", a);
+   }
 
-	int i, j; 
-	for (i = 2; i < 20; i++) {
-		for (j = 2; j < 20; j++) {
-			if (i % j ==  0) {
-				if (i == i) {}
-				else {
-					printf("\n%d is not a prime\n", i); 
-					break; 
-				}
-			}
-		}
-	}
-
-
-	return(0);
+   return(0);
 }
 
 
