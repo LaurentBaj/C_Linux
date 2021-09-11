686126007f18f1791fc28fd09e41df2f51595511
LaurentBaj <laurent_baj@hotmail.com>
2021-09-11 06:15:46 -0700

    Could not finish Ex5

--- Exercices_3/Ex5.c
+++ Exercices_3/Ex5.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+	/*	Create random number 
+	srand(time(NULL));
+	int a = rand() % 100;
+	printf("%d", a)*/;
+	
+
+	int i, j; 
+	for (i = 2; i < 20; i++) {
+		for (j = 2; j < 20; j++) {
+			if (i % j ==  0) {
+				if (i == i) {}
+				else {
+					printf("\n%d is not a prime\n", i); 
+					break; 
+				}
+			}
+		}
+	}
 
 
-int sum(int a, int b); 
-
-int main(int argc, char *argv[])
-{
-	int result = sum(atoi(argv[1]), atoi(argv[2])); 
-	printf("%d\n", result); 
-   
-	return 0;
+	return(0);
 }
 
 
-
-int sum(int a, int b) {
-	return (a + b); 
-}
