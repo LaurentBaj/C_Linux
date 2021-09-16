1e9f34302373a80252edab8b3844bc2890087ae8
LaurentBaj <laurent_baj@hotmail.com>
2021-09-16 03:28:51 -0700

    Data types sizes

---
+++ basics/data_sizes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+   printf("%ld\n", sizeof(int));  // 4
+   printf("%ld\n", sizeof(int*)); // 8
+   printf("%ld\n", sizeof(char)); // 1
+ 
+   return 0; 
+}
+
+
+
