6d5bee94b9c52336d653998399da836ab030edd7
Laurent <laurent_baj@hotmail.com>
2021-10-25 12:45:31 +0200

    Exercices 7

---
+++ Exercices/Exercices_7/Ex1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "test/tools.h"
+
+
+int main(void)
+{
+    for (int i = 0; i < 10; i++) func(); 
+    return 0; 
+} 
---
+++ Exercices/Exercices_7/Ex1/test/tools.h
@@ -0,0 +1,7 @@
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+
+void func (void); 
+
+#endif
