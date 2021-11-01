da5e84588eada53462bd42d9f5dba6551af8f282
Laurent <laurent_baj@hotmail.com>
2021-11-01 15:30:55 +0100

    LinkedList append

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -2,29 +2,59 @@
 #include <stdlib.h>
 
 
+
+
 typedef struct Node
 {
     int x; 
     struct Node* next; 
 } Node; 
 
+
+void append(Node** root, int value)
+{
+    // Create node based on args  
+    Node* new_node = malloc(sizeof(Node)); 
+    if (new_node == NULL)
+    {
+        exit(1);     
+    }
+    new_node->next = NULL; 
+    new_node->x = value; 
+
+    // If Linked List is empty
+    if (*root == NULL) 
+    {
+        *root = new_node; 
+        return; 
+    }
+
+    // Traverse to tail and append 
+    Node* curr = *root; 
+    while (curr->next != NULL)
+    {
+        curr = curr->next;  
+    }
+    curr->next = new_node; 
+}
+
+
 int main()
 {
-    Node root; 
-    root.x = 15; 
-    root.next = malloc(sizeof(Node));  
-    root.next->x = -2;
-    root.next->next = malloc(sizeof(Node));
-    root.next->next->x = 22; 
-    root.next->next->next = NULL; 
+    Node* root = NULL; 
 
-    for (Node* curr = &root; curr != NULL; curr = curr->next)
+    /*if (root == NULL) exit(2);  
+    root->x = 15; 
+    root->next = NULL;*/
+
+    append(&root, -2);
+    append(&root, 3);
+    append(&root, 19);
+
+    for (Node* curr = root; curr != NULL; curr = curr->next)
     {
         printf("%d\n", curr->x);
     }
-
-    free(root.next->next);
-    free(root.next);
 
     return 0; 
 }
