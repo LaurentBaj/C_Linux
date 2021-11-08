8b782ba5cbc12b0fa368511764933483041a23f1
Laurent <laurent_baj@hotmail.com>
2021-11-08 12:17:25 +0100

    Doubly Linked List
    Insert after, delete and find

--- DoublyLinkedList/main.c
+++ DoublyLinkedList/main.c
@@ -26,6 +26,7 @@ void deallocate(Node** tail, Node** head)
     *head = NULL;
 }
 
+
 void insert_beginning(Node** tail, int value)
 {
     Node* new_node = malloc(sizeof(Node)); 
@@ -81,15 +82,79 @@ void append(Node** head, int value)
 }
 
 
+void insert_after(Node* node, int value)
+{
+    Node* new_node = malloc(sizeof(Node)); 
+    if (new_node == NULL) 
+    {
+        exit(4); 
+        return; 
+    }
+    
+    new_node->x = value; 
+    new_node->prev = node; 
+    new_node->next = node->next;
+
+    if (node->next != NULL)
+    {
+        node->next->prev = new_node; 
+    }
+    node->next = new_node; 
+}
+
+
+void remove_node(Node* node)
+{
+    if (node->prev != NULL)
+    {
+        node->prev->next = node->next;
+    }
+    if (node->next != NULL)
+    {
+        node->next->prev = node->prev; 
+    }
+
+    free(node); 
+}
+
+
+Node* find_node(Node* tail, int value)
+{
+    for (Node* curr = tail; curr != NULL; curr = curr->next)
+    {
+        if (curr->x == value) return curr; 
+    } 
+    return NULL; 
+}
+
+
+
 int main()
 {
     Node* tail = NULL; 
-    Node* head = tail; 
+    Node* head = NULL; 
     
     init(&tail, &head, 7); 
     insert_beginning(&tail, 3); 
     insert_beginning(&tail, 1); 
-    append(&head, 5); 
+    insert_after(tail->next, 5); 
+    remove_node(tail->next); 
+    
+    /* Need to do this upon removal of tail or head
+    Node* aux = tail->next; 
+    remove_node(tail); 
+    tail = aux;  */
+
+    Node* found = find_node(tail, 4); 
+    if (found == NULL)
+    {
+        printf("Node was not found\n"); 
+
+    }
+    else 
+    {
+        printf("\nValue: %d; Next: %p\n", found->x, found->next); 
+    }
 
     for (Node* curr = tail; curr != NULL; curr = curr->next)
     {
