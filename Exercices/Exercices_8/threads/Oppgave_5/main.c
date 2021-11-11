4b2d769c8cf1c11e32a1fdd10214c7392da9e8e8
Laurent <laurent_baj@hotmail.com>
2021-11-11 20:51:03 +0100

    Kinda finished

--- Exercices/Exercices_8/threads/Oppgave_5/main.c
+++ Exercices/Exercices_8/threads/Oppgave_5/main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 
-char str[10];
+char str[2], buffer, quit[] = "Quit";
+FILE *file;
+pthread_t main_thread, working_thread;
 
-void *routine(void *arg)
+void *write_to_file(void *arg)
 {
     FILE *temp = (FILE *)arg;
     temp = fopen("text.txt", "a");
@@ -14,28 +17,40 @@ void *routine(void *arg)
     fclose(temp);
 }
 
-int main()
+void *program()
 {
-    FILE *f1;
-    pthread_t t1, t2;
-    char buffer;
-
     do
     {
-        printf("Enter text: ");
-        scanf("%s", str);
+        printf("Add new line to file (a) or quit (q)\n");
+        scanf("%c", &buffer);
 
-        if (pthread_create(&t1, NULL, &routine, f1) != 0)
+        if (buffer == 'q')
         {
-            return 1;
+            break;
         }
-    } while (buffer != 'q');
+        else if (buffer == 'a')
+        {
+            printf("Enter text: ");
+            scanf("%s", str);
+            if (strlen(str) < 10)
+            {
+                pthread_create(&working_thread, NULL, &write_to_file, file);
+            }
+            else
+            {
+                printf("Text can't contain more than 10 letters\n");
+            }
+        }
 
-    if (pthread_join(t1, NULL) != 0)
-    {
-        return 2;
-    }
+    } while (1);
 
-    printf("%c\n", buffer);
+    pthread_join(working_thread, NULL);
+}
+
+int main()
+{
+    pthread_create(&main_thread, NULL, &program, NULL);
+    pthread_join(main_thread, NULL);
+
     return 0;
 }
